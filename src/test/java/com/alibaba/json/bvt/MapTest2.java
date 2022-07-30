dd404e7f89e9bcb7cf5bef5bde3f81b82d64cc92
alexone95 <alessandropontis95@gmail.com>
2022-07-30 12:56:56 +0200

    Tests removed

--- src/test/java/com/alibaba/json/bvt/MapTest2.java
+++ src/test/java/com/alibaba/json/bvt/MapTest2.java
@@ -10,10 +10,10 @@ import com.alibaba.fastjson.TypeReference;
 
 
 public class MapTest2 extends TestCase {
-    public void test_map () throws Exception {
-Map<Object, Object> map = JSON.parseObject("{1:\"2\",\"3\":4,'5':6}", new TypeReference<Map<Object, Object>>() {});
-Assert.assertEquals("2", map.get(1));
-Assert.assertEquals(4, map.get("3"));
-Assert.assertEquals(6, map.get("5"));
+public void test_map () throws Exception {
+        Map<Object, Object> map = JSON.parseObject("{1:\"2\",\"3\":4,'5':6}", new TypeReference<Map<Object, Object>>() {});
+        Assert.assertEquals("2", map.get(1));
+        Assert.assertEquals(4, map.get("3"));
+        Assert.assertEquals(6, map.get("5"));
     }
 }
